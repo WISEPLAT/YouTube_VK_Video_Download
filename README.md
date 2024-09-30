81f51a784ed6066423ab6a40108f6a0985aa11bf
Oleg Shpagin <info@wiseplat.org>
2024-09-30 21:09:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+### Python script for downloading videos from YouTube and VK (GUI) 
+#### Version: 1.5+ (current)
+
+![app screenshot](https://raw.githubusercontent.com/WISEPLAT/YouTube_VK_Video_Download/main/app_screen.jpg)
+
+
+### UPD: April 2 2024 Version 1.5
+The code has been redone using yt-dlp, downloading open videos is available again. 
+The download_video function runs in a separate thread, which allows the main program execution thread to update the interface state. During the download process, the current information is displayed in the interface.
+
+### What's it?
+
+Python script for downloading videos from YouTube and VK via GUI.  
+
+
+### How do I start using it?
+
+1. Download the archive with the latest version and unzip it to any place convenient for you; 
+
+2. Install the necessary components and dependencies if necessary;
+
+3. Run the file (script) 
+
+*youtube_vk_video_download.py* - to download videos in threads;
+
+or *youtube_vk_video_download_no_threads.py* - to download videos NOT in threads (to be not Banned);
+
+4. In the window that appears, paste the link to the video in the input field (or links - separated with comma) and click *Download video*;
+
+5. In case of successful download, a notification will appear, and the video will be saved in the *downloads* folder
+
+> For YouTube, the links should look like: *https://www.youtube.com/shorts/L-F7stkCG6A* or *https://www.youtube.com/watch?v=RoJhyg8m8Rg*
+![youtube downloads](https://raw.githubusercontent.com/WISEPLAT/YouTube_VK_Video_Download/main/youtube_downloads.jpg)
+
+> For VK, the links should look like: *https://vk.com/video-174522765_456239108* 
+![vk downloads](https://raw.githubusercontent.com/WISEPLAT/YouTube_VK_Video_Download/main/vk_downloads.jpg)
+
+
+### Possible problems
+
+- The link is not inserted into the input field
+> You probably have the Russian keyboard layout enabled, switch to English.
+
+- When the program window is closed, the console continues to run
+> The bug is related to ongoing processes in the background, it will be resolved in the future
+
+- The application (script) freezes during download
+> This is a feature of the work, at this time there is a download in the background, at the end of the download the script will hang itself.
+
+- The video does not download
+> The video is either closed to unauthorized access or has a format\source not supported by yt-dlp
+
+- The files have a format after downloading .unknown_video
+> Rename to .mp4? as a rule, this is enough to solve the problem
+
+- Where is the finished video downloaded?
+> To the downloads directory, located in the same place where your script is located vk_video_download.py
+
+---
+
+The command to install the necessary components
+
+    pip install -r requirements.txt
+    
+Or a separate installation of yt-dlp
+
+    python3 -m pip install -U yt-dlp
+    
+The command to build the exe file in pyinstaller:
+
+    pyinstaller vk_video_download.py --noconsole --onefile --icon=icon.ico
+ 
+### Copyrights and Licenses
+Not for commercial use.
+
+### Did you find this useful?! => Set a star))
+
+## Thanks
+Thanks for initial source code to github.com/blyamur
---
+++ youtube_vk_video_download_no_threads.py
@@ -0,0 +1,170 @@
+# pip install yt_dlp requests requests
+
+import threading
+import tkinter as tk
+import webbrowser
+from tkinter import ttk, messagebox
+import yt_dlp
+import requests
+from datetime import datetime
+
+currentVersion = '1.5+'
+class App(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self)
+        for index in [0, 1, 2]:
+            self.columnconfigure(index=index, weight=1)
+            self.rowconfigure(index=index, weight=1)
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        self.widgets_frame = ttk.Frame(self, padding=(0, 10, 0, 0))
+        self.widgets_frame.grid(
+            row=0, column=1, padx=10, pady=(25, 0), sticky="nsew"
+        )
+        self.widgets_frame.columnconfigure(index=0, weight=1)
+        self.label = ttk.Label(
+            self.widgets_frame,
+            text="Insert the link(s) to the video separated by commas (No Threads)",
+            justify="center",
+            font=("-size", 15, "-weight", "bold"),
+        )
+        self.label.grid(row=0, column=0,padx=0, pady=25, sticky="n")
+
+        self.entry_nm = ttk.Entry(self.widgets_frame, font=("Calibri 22"))
+        self.entry_nm.insert(tk.END, str(''))
+        self.entry_nm.grid(row=1, column=0, columnspan=10, padx=(5, 5), ipadx=150, ipady=5, pady=(0, 0), sticky="ew")
+        self.entry_nm.bind('<Return>', self.on_enter_pressed)
+
+        self.bt_frame = ttk.Frame(self, padding=(0, 0, 0, 0))
+        self.bt_frame.grid(row=1, column=0, padx=(10, 10), pady=0, columnspan=10, sticky="n")
+
+        self.accentbutton = ttk.Button(
+            self.bt_frame, text="Download video", style="Accent.TButton",command=self.get_directory_string
+        )
+        self.accentbutton.grid(row=0, column=0,columnspan=3, ipadx=30, padx=2, pady=(5, 0), sticky="n")
+
+        self.bt_frame.columnconfigure(index=0, weight=1)
+        self.status_label = ttk.Label(
+            self.bt_frame,
+            text=" ",
+            justify="center",
+            font=("-size", 10, "-weight", "normal"),
+        )
+        self.status_label.grid(row=1, column=0,padx=0, pady=15, sticky="n") 
+
+        self.copy_frame = ttk.Frame(self, padding=(0, 0, 0, 10))
+        self.copy_frame.grid(row=8, column=0, padx=(10, 10), pady=5, columnspan=10 , sticky="s")
+        self.UrlButton = ttk.Button(
+            self.copy_frame, text="About", style="Url.TButton",command=self.openweb
+        )
+        self.UrlButton.grid(row=1, column=0, padx=20, pady=0, columnspan=2, sticky="n")
+        self.UrlButton = ttk.Button(
+            self.copy_frame, text="Vers.: " +currentVersion+" ", style="Url.TButton",command=self.checkUpdate
+        ) 
+        self.UrlButton.grid(row=1, column=4, padx=20, pady=0, columnspan=2, sticky="w")
+        self.UrlButton = ttk.Button(
+            self.copy_frame, text="Donate", style="Url.TButton",command=self.donate
+        )
+        self.UrlButton.grid(row=1, column=7, padx=20, pady=0, columnspan=2, sticky="w")
+
+    def openweb(self):
+        webbrowser.open_new_tab('https://github.com/WISEPLAT/YouTube_VK_Video_Download')
+
+    def donate(self):
+        webbrowser.open_new_tab('https://ko-fi.com/monseg')
+
+    def checkUpdate(self, method='Button'):
+        try:
+            github_page = requests.get('https://raw.githubusercontent.com/WISEPLAT/YouTube_VK_Video_Download/main/README.md')
+            github_page_html = str(github_page.content).split()
+            for i in range(0,8):
+                try:
+                    index = github_page_html.index(('1.' + str(i)))
+                    version = github_page_html[index]
+                except ValueError:
+                    pass
+
+            if float(version) > float(currentVersion):
+                self.updateApp(version)
+            else:
+                if method == 'Button':
+                    messagebox.showinfo(title='No updates found', message=f'No updates found.\nCurrent version: {version}')
+        except requests.exceptions.ConnectionError:
+            if method == 'Button':
+                messagebox.showwarning(title='No network access', message='No network access.\nCheck your internet connection.')
+            elif method == 'ConnectionError':
+                pass
+        except Exception as e:
+                print(f"An error occurred: {e}")
+
+    def updateApp(self, version):
+        update = messagebox.askyesno(title='An update has been found', message=f'A new version is available {version} . Update?')
+        if update:
+            webbrowser.open_new_tab('https://github.com/WISEPLAT/YouTube_VK_Video_Download')
+
+    def get_directory_string(self):
+        if self.entry_nm.get() == '':
+            self.status_label.configure(text="You have not entered a link to the video")
+            pass
+        else:
+            video_urls = self.entry_nm.get().split(',')
+            for video_url in video_urls:
+                video_url = video_url.strip()
+                if video_url:
+                    try:
+                        # t = threading.Thread(target=self.download_video, args=(video_url,))
+                        # t.start()
+                        self.download_video(video_url)
+                    except:
+                        self.status_label.configure(text="Error")
+            self.entry_nm.delete(0, tk.END)
+
+    def download_video(self, video_url):
+        try:
+            #ydl_opts = {'outtmpl': 'downloads/%(title)s.%(ext)s', 'quiet': True, 'progress_hooks': [self.my_hook]}
+
+            # Current date and time, conversion to string
+            dt = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+            ydl_opts = {'outtmpl': f'downloads/{dt}_%(title)s.mp4', 'quiet': True, 'progress_hooks': [self.my_hook]}
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([video_url])
+                info = ydl.extract_info(video_url, download=True)
+                self.status_label.configure(text=f"Video has been downloaded successfully: «{info['title']}»")
+                print(f"url: {video_url} - Ok")
+        except:
+            self.status_label.configure(text="Error")
+
+    def my_hook(self, d):
+       if d['status'] == 'downloading':
+           percent_str_clear = d['_percent_str'].replace('[0;94m', '')
+           percent_str = percent_str_clear
+           percent_str = ''.join(chr for chr in percent_str if chr.isprintable())
+           percent = percent_str.split('%')[0].strip()
+           root.after(0, lambda: self.status_label.configure(text=f"Downloading... {percent}% ", font=("Arial", 10)))
+       elif d['status'] == 'finished':
+           root.after(0, lambda: self.status_label.configure(text="Download completed!", font=("Arial", 10)))
+
+
+    def on_enter_pressed(self, event):
+        self.get_directory_string()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+    w = w//2 
+    h = h//2 
+    w = w - 200
+    h = h - 200
+    root.geometry('680x350+{}+{}'.format(w, h))
+    root.resizable(False, False)
+    root.title("Download video from YouTube or VK (no Threads => no Bans)")
+    root.iconbitmap('theme/icon.ico')
+    root.tk.call("source", "theme/vk_theme.tcl")
+    root.tk.call("set_theme", "light")
+    app = App(root)
+    app.pack(fill="both", expand=True)
+    root.update()
+    root.mainloop()
